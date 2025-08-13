73857c9f302dcb1ecb32f4a51ce330cd5e5f155b
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-08-13 16:20:27 +0000

    Add functionality to add task btn

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const addButton = document.getElementById('add-task-btn');
+
+    const taskInput = document.getElementById('task-input');
+
+    const taskList = document.getElementById('task-list');
+
+
+    const addTask = () => {
+        const taskText = taskInput.value.trim()
+
+        if (!taskText) {
+            alert('Enter a task here');
+        } 
+
+        const taskItem = document.createElement('li');
+        taskItem.textContent = taskText;
+
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Remove';
+        removeBtn.className = 'remove-btn';
+
+        removeBtn.addEventListener('click', () => {
+            removeBtn.parentElement.remove();
+        });
+
+        taskItem.appendChild(removeBtn);
+        taskInput.value = "";
+        
+    }
+
+}); 
